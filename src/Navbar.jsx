45430439d3c0e2665b0731bf1ed42bbbca20597c
tidjini <tidjini.messaoudi@gmail.com>
2023-03-10 12:38:01 +0100

    Navigation base style

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,10 +8,26 @@ export default function Navbar() {
       </div>
       <nav className="header-section">
         <ul className="flex-center">
-          <li>01. About</li>
-          <li>02. Projects</li>
-          <li>03. Cv</li>
-          <li>04. Contacts</li>
+          <li className="nav-item active">
+            <a href="#about">
+              <span className="green">01.</span> About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#projects">
+              <span className="green">02.</span> Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#cv">
+              <span className="green">03.</span> Cv
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#contacts">
+              <span className="green">04.</span> Contacts
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
