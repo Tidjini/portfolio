36b40893e0c5f6020e70435561dfe88bf1a07b2a
tidjini <tidjini.messaoudi@gmail.com>
2023-03-10 11:56:56 +0100

    add Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header>
+      <img id="logo" src="assets/tidjini.messaoudi.me.png" />
+      <nav>
+        <ul>
+          <li>01. About</li>
+          <li>02. Projects</li>
+          <li>03. Cv</li>
+          <li>04. Contacts</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
