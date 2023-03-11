cba17643f984893f4510b12a9361238e6f1d7e98
tidjini <tidjini.messaoudi@gmail.com>
2023-03-11 09:38:31 +0100

    add main section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar.jsx";
+import Main from "./Main.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
---
+++ src/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <section>
+      <h1>Hello,</h1>
+      <h1>
+        I am <span className="green">Tidjini</span>,
+      </h1>
+      <h1>Fullstack Developer</h1>
+      <p>
+        Currently locatated in Algeria, I love building{" "}
+        <span className="green">interactive</span>, digital experiences on the
+        web, mobile, and desktop.
+      </p>
+      <button>let's get in touch!</button>
+    </section>
+  );
+}
