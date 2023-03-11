833f9278c764314a38f2acccae61ff2301318024
tidjini <tidjini.messaoudi@gmail.com>
2023-03-11 09:38:20 +0100

    set main section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navbar from "./Navbar.jsx";
-ReactDOM.createRoot(document.getElementById("root")).render(<Navbar />);
+import App from "./App.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
