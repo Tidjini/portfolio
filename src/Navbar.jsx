41bc962773b2ff863da43238bb38b178d76cf015
Tidjini <tidjini.messaoudi@gmail.com>
2023-03-12 19:32:22 +0100

    menu display

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,21 @@
-import Hidden from "@mui/material/Hidden";
 import React from "react";
+import Hidden from "@mui/material/Hidden";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <header className="flex-center">
       <div className="header-section">
@@ -40,6 +54,55 @@ export default function Navbar() {
             </li>
           </ul>
         </nav>
+      </Hidden>
+      <Hidden mdUp>
+        <IconButton
+          id="menu-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MenuIcon style={{ color: "white" }}></MenuIcon>
+        </IconButton>
+        <Menu
+          id="men"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "menu-button",
+          }}
+          PaperProps={{
+            style: {
+              backgroundColor: "#141419",
+            },
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <a href="#about" className="nav-item active">
+              <span className="green">01.</span> About
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a href="#projects" className="nav-item">
+              <span className="green">02.</span> Projects
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a href="#contacts" className="nav-item">
+              <span className="green">03.</span> Contacts
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a href="#cv" className="download">
+              download CV{" "}
+              <span className="open-box">
+                <span className="arrow"></span>
+              </span>
+            </a>
+          </MenuItem>
+        </Menu>
       </Hidden>
     </header>
   );
